0eb313dc70e38b193e9e5e441c3057408c55e2b5
yuina.fukuoka <yuina.fukuoka@rakuten.com>
2018-06-18 01:39:25 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ Route::get('/', function () {
 });
 
 Route::get('/', 'WelcomeController@index');
+
+// User registration
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+
