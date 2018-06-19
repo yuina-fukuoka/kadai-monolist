f443ad37773b107bc93d428be1b091854d72067e
yuina.fukuoka <yuina.fukuoka@rakuten.com>
2018-06-19 04:38:27 +0000

    correct2-1

--- resources/views/ranking/have.blade.php
+++ resources/views/ranking/have.blade.php
@@ -2,5 +2,39 @@
 
 @section('content')
     <h1>Haveランキング</h1>
-    @include('items.items', ['items' => $items])
+   @if ($items)
+    <div class="row">
+        @foreach ($items as $key=>$item)
+            <div class="item">
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading text-center">
+                            <img src="{{ $item->image_url }}" alt="" class="">
+                        </div>
+                        <div class="panel-body">
+                             @if ($item->id)
+                                <p class="item-title"><a href="{{ route('items.show', $item->id) }}">{{ $item->name }}</a></p>
+                            @else
+                                <p class="item-title">{{ $item->name }}</p>
+                            @endif
+                            <div class="buttons text-center">
+                                @if (Auth::check())
+                                    @include('items.want_button', ['item' => $item])
+                                
+                                    @include('items.have_button', ['item' => $item])
+                                @endif
+                            </div>
+                        </div>
+                        @if (isset($item->count))
+                            <div class="panel-footer">
+                                
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Haves</p>
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endif
 @endsection
